0eafb1d1af25a02dc76578eac8823d3033f7d957
CritzyInf <pedroh.srodrigues6@gmail.com>
2025-02-24 20:48:02 -0300

    primeiro site

---
+++ VSCODE/JS/scripts.js
@@ -0,0 +1,9 @@
+//função que chama o HTML//
+
+
+    function ContaLink(){
+   window.location='https://pt.wikipedia.org/wiki/Nymphicus_hollandicus';
+    }
+    function ContaGit(){
+        window.location='https://github.com/CritzyInfer';
+         }
