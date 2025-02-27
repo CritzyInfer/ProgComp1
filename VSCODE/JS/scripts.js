9d031e02ddacd04dfe54325c608723449f5402a4
CritzyInf <pedroh.srodrigues6@gmail.com>
2025-02-26 21:41:49 -0300

    teste

--- VSCODE/JS/scripts.js
+++
@@ -1,9 +0,0 @@
-//função que chama o HTML//
-
-
-    function ContaLink(){
-   window.location='https://pt.wikipedia.org/wiki/Nymphicus_hollandicus';
-    }
-    function ContaGit(){
-    window.location='https://github.com/CritzyInfer';
-         }
